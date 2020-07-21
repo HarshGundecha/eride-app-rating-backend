79e8fd9f61dd58bcd7e6963a999ccf8a02bb9e71
HarshGundecha <harsh.gundecha@gmail.com>
2020-07-21 20:12:26 +0530

    initiate springboot app

---
+++ src/main/java/com/happysolutions/erideappratingbackend/ERideAppRatingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.happysolutions.erideappratingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERideAppRatingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERideAppRatingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happysolutions/erideappratingbackend/ERideAppRatingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happysolutions.erideappratingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERideAppRatingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
